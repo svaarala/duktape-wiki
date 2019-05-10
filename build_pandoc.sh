7f750d3d21205e77aba5254ae5eb8930b63d6c7d
Sami Vaarala <sami.vaarala@iki.fi>
2019-05-10 14:40:55 +0300

    Rework build script for Docker
    Also remove old gollum script.

--- build_pandoc.sh
+++ build_pandoc.sh
@@ -1,81 +1,72 @@
-STATIC_SITE=/srv/duktape-wiki
 MARKUP=markdown  # commonmark
+ENTRYWD=`pwd`
+
+DUKTAPE_WIKI_REPO=$ENTRYWD
+DUKTAPE_REPO=$ENTRYWD/duktape
 
 set -e  # exit on error
 
-if [ ! -d $STATIC_SITE ]; then
-    echo "Invalid static site: $STATIC_SITE"
-fi
-
 cd /tmp
-rm -rf wiki-rebuild-tmp
+rm -rf wiki-build-tmp
 rm -rf wiki-output-tmp
-mkdir wiki-rebuild-tmp
+mkdir wiki-build-tmp
 mkdir wiki-output-tmp
 
-# Fresh clone of duktape-wiki repository (the wiki is quite small)
-cd /tmp/wiki-rebuild-tmp
-git clone --depth 1 https://github.com/svaarala/duktape-wiki.git
-#cp -r /home/duktape/duktape-wiki /tmp/wiki-rebuild-tmp/
-
-# Fresh clone of duktape repository
-cd /tmp/wiki-rebuild-tmp
-git clone --depth 1 https://github.com/svaarala/duktape.git
-
 # Genconfig generated files
-cd /tmp/wiki-rebuild-tmp/duktape
+cd $DUKTAPE_REPO
 python tools/genconfig.py \
 	--metadata config \
-	--output /tmp/wiki-rebuild-tmp/duktape-wiki/ConfigOptions.rst \
+	--output /tmp/wiki-build-tmp/ConfigOptions.rst \
 	config-documentation
 #python tools/genconfig.py \
 #	--metadata config \
-#	--output /tmp/wiki-rebuild-tmp/duktape-wiki/FeatureOptions.rst \
+#	--output /tmp/wiki-build-tmp/FeatureOptions.rst \
 #	feature-documentation
 
 # Internal doc/ files are .rst and supported by pandoc, copy them over
-cd /tmp/wiki-rebuild-tmp/duktape
-cp doc/*.rst /tmp/wiki-rebuild-tmp/duktape-wiki/
+cd $DUKTAPE_REPO
+cp doc/*.rst /tmp/wiki-build-tmp/
+
+# Copy Wiki files over.
+cd $DUKTAPE_WIKI_REPO
+cp *.md /tmp/wiki-build-tmp/
 
 # Convert wiki links to pandoc compatible ones.
-cd /tmp/wiki-rebuild-tmp/duktape-wiki
-python convert_links.py *.md
+cd /tmp/wiki-build-tmp/
+python $DUKTAPE_WIKI_REPO/convert_links.py *.md
 
-cd /tmp/wiki-rebuild-tmp/duktape-wiki
-for fn in /tmp/wiki-rebuild-tmp/duktape-wiki/*.md; do
+cd /tmp/wiki-build-tmp/
+for fn in /tmp/wiki-build-tmp/*.md; do
     # --number-sections ?
     # --toc ?
     # --title ?
     echo $fn
+    echo $fn >&2
     pandoc -r $MARKUP --highlight-style haddock -w html5 \
         --css style-top.css \
         --css github-pandoc-adapted.css \
-        --include-in-header in_header.html \
-        --include-before-body before_body.html \
-        --include-after-body after_body.html \
+        --include-in-header $DUKTAPE_WIKI_REPO/in_header.html \
+        --include-before-body $DUKTAPE_WIKI_REPO/before_body.html \
+        --include-after-body $DUKTAPE_WIKI_REPO/after_body.html \
         -o "${fn%%.md}.html" "$fn"
 done
-for fn in /tmp/wiki-rebuild-tmp/duktape-wiki/*.rst; do
+for fn in /tmp/wiki-build-tmp/*.rst; do
     # --number-sections ?
     # --toc ?
     # --title ?
     echo $fn
+    echo $fn >&2
     pandoc -r rst --highlight-style haddock -w html5 \
         --css style-top.css \
         --css github-pandoc-adapted.css \
-        --include-in-header in_header.html \
-        --include-before-body before_body.html \
-        --include-after-body after_body.html \
+        --include-in-header $DUKTAPE_WIKI_REPO/in_header.html \
+        --include-before-body $DUKTAPE_WIKI_REPO/before_body.html \
+        --include-after-body $DUKTAPE_WIKI_REPO/after_body.html \
         -o "${fn%%.rst}.html" "$fn"
 done
 
-cp /tmp/wiki-rebuild-tmp/duktape-wiki/*.html /tmp/wiki-output-tmp/
-cp /tmp/wiki-rebuild-tmp/duktape/website/style-top.css /tmp/wiki-output-tmp/
-cp /tmp/wiki-rebuild-tmp/duktape-wiki/github-pandoc-adapted.css /tmp/wiki-output-tmp/
+cp /tmp/wiki-build-tmp/*.html /tmp/wiki-output-tmp/
+cp $DUKTAPE_REPO/website/style-top.css /tmp/wiki-output-tmp/
+cp $DUKTAPE_WIKI_REPO/github-pandoc-adapted.css /tmp/wiki-output-tmp/
 
-# Copy final site on success to statically served site
-if [ ! -d $STATIC_SITE ]; then
-    echo "Invalid static site: $STATIC_SITE"
-fi
-rm -rf $STATIC_SITE/*
-cp -r /tmp/wiki-output-tmp/* $STATIC_SITE/
+ls -l /tmp/wiki-output-tmp
